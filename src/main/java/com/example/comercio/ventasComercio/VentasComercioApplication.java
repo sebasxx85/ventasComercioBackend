999c64952f6ba37b447beb25c30a17c72009c041
sebasxx85 <102833994+sebasxx85@users.noreply.github.com>
2024-07-08 16:59:17 -0400

    Add files via upload
    add files manual

---
+++ src/main/java/com/example/comercio/ventasComercio/VentasComercioApplication.java
@@ -0,0 +1,13 @@
+package com.example.comercio.ventasComercio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasComercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasComercioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/comercio/ventasComercio/VentasComercioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comercio.ventasComercio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasComercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
