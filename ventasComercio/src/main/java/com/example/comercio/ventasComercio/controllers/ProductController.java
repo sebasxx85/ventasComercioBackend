083216861eb5165ffedeb4fd7f8775c4cdb2dff9
sebaxx85 <isurus.1313@gmail.com>
2024-07-08 21:29:58 -0400

    add create product

--- ventasComercio/src/main/java/com/example/comercio/ventasComercio/controllers/ProductController.java
+++ ventasComercio/src/main/java/com/example/comercio/ventasComercio/controllers/ProductController.java
@@ -1,8 +1,11 @@
 package com.example.comercio.ventasComercio.controllers;
 
+import com.example.comercio.ventasComercio.entities.Product;
 import com.example.comercio.ventasComercio.servicies.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("api/v1/products")
@@ -11,6 +14,16 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    //Crear Producto
+    @PostMapping()
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        try {
+            return new ResponseEntity<>(service.save(product), HttpStatus.CREATED);
+        } catch (Exception e) {
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
 
 }
--- ventasComercio/src/main/java/com/example/comercio/ventasComercio/servicies/ProductService.java
+++ ventasComercio/src/main/java/com/example/comercio/ventasComercio/servicies/ProductService.java
@@ -1,5 +1,7 @@
 package com.example.comercio.ventasComercio.servicies;
 
+
+import com.example.comercio.ventasComercio.entities.Product;
 import com.example.comercio.ventasComercio.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,5 +10,9 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     @Autowired
     private ProductRepository repository;
+
+    public Product save(Product product){
+        return repository.save(product);
+    }
 
 }
